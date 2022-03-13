46e4efe4bb3ffd9d5d904e3e11f81bae84b9a8e5
DanteGRP <100560621+DanteGRP@users.noreply.github.com>
2022-03-14 00:09:43 +1000

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,13 @@
+const $portfolio = document.querySelector(".sec-portfolio-js");
+const $modalImgPortfolio = document.querySelector(".img-modal-js");
+
+$portfolio.addEventListener("click", (e)=>{
+	console.log(e.target.classList);
+	if(e.target.classList.contains("img-btn-modal-js")){
+		// Src
+		let urlImg = e.target.attributes[0].nodeValue;
+		
+		// Add Modal
+		$modalImgPortfolio.src = urlImg;
+	}
+})
